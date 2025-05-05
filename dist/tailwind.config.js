7ce2abc835d964b38e1b96cbaa3c1cc1bf58aa16
wildREA <wildrealinit@gmail.com>
2025-05-05 14:41:09 +0200

    Ignore LFS-related files

---
+++ dist/tailwind.config.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/** @type {import('tailwindcss').Config} */
+exports.default = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
