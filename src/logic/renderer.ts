a90ae674effca6c51abc140278422aba34a826ce
wildREA <wildrealinit@gmail.com>
2025-05-07 10:33:50 +0200

    Add variables and Default Behavior prevention

--- src/logic/renderer.ts
+++ src/logic/renderer.ts
@@ -1,5 +1,17 @@
 // This file is used to render the logic of the app
 // Importing immigrants
-import { a, b, add } from './utils/test.js';
+import { a, b, add } from "./utils/test.js";
 
 add(a, b);
+
+// Importing the logic
+import { ipcRenderer } from "electron";
+const dropzone = document.getElementById("dropzone-file") as HTMLDivElement;
+const downloads = document.getElementById("download-section") as HTMLDivElement;
+const compressed = document.getElementById(
+  "compression-results"
+) as HTMLDivElement;
+
+["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
+  dropzone.addEventListener(eventName, (e) => e.preventDefault());
+});
